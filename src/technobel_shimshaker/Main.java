493851126e34b0a9520f1668acb3ec2eb85c7f80
Shimshaker <shimshaker@gmail.com>
2021-08-25 12:18:03 +0200

    Just looking for JDBC Pilotes installation...

--- src/technobel_shimshaker/Main.java
+++ src/technobel_shimshaker/Main.java
@@ -6,21 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-//            ContactDAO cd = new ContactDAOImpl();
-//
-//            cd.getAll();
-//
-//            cd.getOne("09aa2484-4894-4f3e-b238-01858445cd07");
-//
-//            Contact test = new Contact("121312313213", "Niouc","Targa", "Jackouille","25/05/1947", "Belge", "Rue de poule",5,6,"2565", "Belgique", "+324564/45", "12332154545", "jachk@gmail.com", "Bouile");
-//
-//            cd.insert(test);
-//
-//            cd.getAll();
-//
-//            cd.delete("121312313213");
-//
-//            cd.getAll();
         InterfaceConsole ic = new InterfaceConsole();
         ic.start();
     }
